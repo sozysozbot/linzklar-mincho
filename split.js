33b9afebb0b130eefdfee3d01428aee47eb424bf
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2023-01-13 20:57:14 +0900

    Add `xmlns:sodipodi`

--- split.js
+++ split.js
@@ -31,7 +31,7 @@ const dom = new JSDOM(content);
 dom.window.document.querySelectorAll("g").forEach(g => {
     if (/^g[0-9A-Fa-f]{4}_[a-zA-Z0-9á_.]*$/.test(g.id)) {
         fs.writeFileSync(`glyphs/${g.id}.svg`, `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg viewBox="0 0 2000 2000" version="1.1" xmlns="http://www.w3.org/2000/svg">
+<svg viewBox="0 0 2000 2000" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd">
 	${g.innerHTML}
 </svg>
 `);
