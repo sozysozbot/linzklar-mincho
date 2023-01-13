3427f6a34a40ae3189657ef6ecddc8356ad1237d
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2023-01-13 20:40:23 +0900

    decomposition successful

--- split.ts
+++ split.ts
@@ -10,9 +10,14 @@ import * as jsdom from 'jsdom';
 	const dom = new JSDOM(content);
 	dom.window.document.querySelectorAll("g").forEach(g => {
 		if (/^g[0-9A-Fa-f]{4}_[a-zA-Z0-9á_.]*$/.test(g.id)) {
-			// todo
+			fs.writeFileSync(`glyphs/${g.id}.svg`, `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg viewBox="0 0 2000 2000" version="1.1" xmlns="http://www.w3.org/2000/svg">
+	${g.innerHTML}
+</svg>
+`);
+			console.log(`wrote to glyphs/${g.id}.svg`);
 		} else {
-			console.log(g, g.id);
+			console.log("skipped:", g, g.id);
 		}
 	})
 })();
